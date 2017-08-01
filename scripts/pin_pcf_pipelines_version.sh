9325a02588b4666e7396e2c77e8abd7382c2c4be
Kris Hicks <khicks@pivotal.io>
2017-08-01 12:24:27 -0700

    WIP - add pin pcf pipelines git pipeline

---
+++ scripts/pin_pcf_pipelines_version.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+set -eu
+set -o pipefail
+
+overwrite=""
+
+while getopts v:w: option; do
+ case "${option}" in
+ v)
+   version=${OPTARG};;
+ w)
+   overwrite=${OPTARG:-"false"};;
+ esac
+done
+
+root="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ $(fly -h 2>&1 | grep fmt -c) ]]; then
+  has_fly_fmt=0
+else
+  has_fly_fmt=1
+fi
+
+echo "Will pin pcf-pipelines to ${version}"
+
+test_for_pcf_pipelines_git=$(cat <<-EOF
+- op: test
+  path: /resources/name=pcf-pipelines
+  value:
+    name: pcf-pipelines
+    type: git
+    source:
+      uri: git@github.com:pivotal-cf/pcf-pipelines.git
+      branch: master
+      private_key: {{git_private_key}}
+EOF
+)
+
+pin_pcf_pipelines=$(cat <<-EOF
+- op: add
+  path: /get=pcf-pipelines/version
+  value:
+    ref: ${version}
+EOF
+)
+
+files=$(
+  find \
+    "$root/.." \
+    -type f \
+    -name pipeline.yml |
+  grep -v ci
+)
+
+for f in ${files[@]}; do
+  if [[ $( cat $f | yaml-patch -o <(echo "$test_for_pcf_pipelines_git") 2>/dev/null ) ]]; then
+    echo "Pinning ${f}"
+    cat $f |
+    yaml-patch -o <(echo "$pin_pcf_pipelines") > "${f}.pinned"
+
+    if [[ "${overwrite}" == "true" ]]; then
+      filename=$f
+    else
+      filename=${f/.yml/}-pinned.yml
+    fi
+
+    mv "${f}.pinned" $filename
+
+    if [[ $has_fly_fmt == 0 ]]; then
+      fly fmt --write --config $filename
+    fi
+  fi
+done
